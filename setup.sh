e71b966addfe3cae7e8b474684ac04898737bf2a
Donghun Choi <d47choi@uwaterloo.ca>
2026-01-08 16:24:53 -0500

    updated setup.md for debian linux[skip ci]

--- setup.sh
+++ setup.sh
@@ -6,26 +6,25 @@ set -e
 sudo apt-get update && sudo apt-get install -y --no-install-recommends \
     gcc
 
-
-# ** 데비안 계열이라 https://docs.docker.com/engine/install/debian/#install-using-the-repository  이 필ㅛ
-# install docker
-sudo apt-get install -y ca-certificates curl 
+sudo apt update -y
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
-sudo apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-rm -rf /var/lib/apt/lists/*
+sudo apt update -y
 
 sudo docker run -d \
   --name my-app \
+  -p 5000:5000 \
   --restart always \
   --privileged \
   -e ARDUINO_PORT=/dev/ttyACM1 \
